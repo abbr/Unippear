2ab6a3b3d85083ae8090da604603b324b1e1f1e0
abbr <wenfred@gmail.com>
2014-12-06 21:50:58 +0000

    added a test html page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/index.js', function(req, res) {
+    res.type('application/javascript');
+    res.render('index', {
+        title: req.get('host')
+    });
 });
 
 module.exports = router;
