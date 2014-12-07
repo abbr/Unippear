72f09653d7032af747900d85891154149abd33dd
abbr <wenfred@gmail.com>
2014-12-07 05:23:35 +0000

    enumerate over js folder to load all .js files

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,20 @@ var router = express.Router();
 /* GET home page. */
 router.get('/index.js', function(req, res) {
     res.type('application/javascript');
-    res.render('index', {
-        currHost: req.protocol + "://" + req.host
+
+    var recursive = require('recursive-readdir');
+
+    // ignore files named 'foo.cs' or files that end in '.html'.
+    recursive('../public/js', function(err, files) {
+        // Files is an array of filename
+        var relativeFiles = files.map(function(v) {
+            return v.substring(13).replace(/\.js$/,'');
+        });
+        var jsFiles = relativeFiles.join('","');
+        res.render('index', {
+            currHost: req.protocol + "://" + req.host,
+            jsFiles: jsFiles
+        });
     });
 });
 
