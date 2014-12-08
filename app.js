197b7f8a5a304abaefb257d84b9fd1aa94e63e44
abbr <wenfred@gmail.com>
2014-12-08 05:50:08 +0000

    moved static files to public/static folder; allow static files use ejs render engine

--- app.js
+++ app.js
@@ -14,18 +14,33 @@ app.set('views', path.join(__dirname, routes.publicFolderNm));
 app.set('view engine', 'ejs');
 
 // trust proxy
-app.set('trust proxy', true); 
+app.set('trust proxy', true);
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, routes.publicFolderNm)));
+app.use(express.static(path.join(__dirname, routes.publicFolderNm, 'static')));
 
-app.use('/', routes);
+app.use('/index.js', routes);
+
+app.use(function(req, res, next) {
+    require('fs').exists(path.join(__dirname, routes.publicFolderNm, 'static', req.path + '.ejs'), function(exists) {
+        if (exists) {
+            res.render(path.join('static', req.path + '.ejs'), {
+                currHost: req.protocol + "://" + req.host
+            });
+
+        }
+        else {
+            next();
+        }
+    });
+});
+
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,7 +56,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render('api/error', {
             message: err.message,
             error: err,
             title: 'error'
@@ -53,7 +68,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('api/error', {
         message: err.message,
         error: {},
         title: 'error'
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/*', function(req, res) {
+router.get('/', function(req, res) {
     res.type('application/javascript');
 
     var recursive = require('recursive-readdir');
 
-    recursive('../' + router.publicFolderNm + '/js', function(err, files) {
+    recursive('../' + router.publicFolderNm + '/static/js', function(err, files) {
         // Files is an array of filename
         var relativeFiles = files.map(function(v) {
-            return v.substring(router.publicFolderNm.length + 7).replace(/\.js$/, '');
+            return v.substring(router.publicFolderNm.length + 14).replace(/\.js(\.ejs)?$/, '');
         });
         var jsFiles = relativeFiles.join('","');
 
-        recursive('../' + router.publicFolderNm + '/css', function(err, files) {
+        recursive('../' + router.publicFolderNm + '/static/css', function(err, files) {
             // Files is an array of filename
             var cssFiles = files.map(function(v) {
-                return req.protocol + "://" + req.host + v.substring(router.publicFolderNm.length + 3);
+                return req.protocol + "://" + req.host + v.substring(router.publicFolderNm.length + 10);
             });
             res.render('api/index', {
                 currHost: req.protocol + "://" + req.host,
