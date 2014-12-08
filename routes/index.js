7948caf07610f4b02678e0e62a21d44c86cb4349
abbr <wenfred@gmail.com>
2014-12-08 19:42:13 +0000

    moved html files up one level to follow conventions

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/', function(req, res) {
         recursive('../' + router.publicFolderNm + '/static/css', function(err, files) {
             // Files is an array of filename
             var cssFiles = files.map(function(v) {
-                return req.protocol + "://" + req.host + v.substring(router.publicFolderNm.length + 10);
+                return req.protocol + "://" + req.host + v.substring(router.publicFolderNm.length + 10).replace(/\.ejs$/, '');
             });
             res.render('api/index', {
                 currHost: req.protocol + "://" + req.host,
