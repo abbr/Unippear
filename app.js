ed08a90f07dc82316da23b950d4087ce9a1b94e8
abbr <wenfred@gmail.com>
2014-12-15 22:36:15 +0000

    add unippear host to client whitelist automatically

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(function(req, res, next) {
     if (req.headers.referer) {
         if (!validClients.some(function(v) {
                 return v.test(req.headers.referer);
-            })) {
+            }) && req.headers.referer.indexOf(req.protocol + "://" + req.host) !== 0) {
             return res.status(403).end();
         }
     }
@@ -49,7 +49,7 @@ app.use(function(req, res, next) {
     if (req.headers.origin) {
         if (validClients.some(function(v) {
                 return v.test(req.headers.origin);
-            })) {
+            }) || req.headers.origin.indexOf(req.protocol + "://" + req.host) === 0) {
             res.header('Access-Control-Allow-Origin', req.headers.origin);
             if (req.headers['access-control-request-method']) {
                 res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
