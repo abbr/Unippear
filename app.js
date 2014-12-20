3dbb3b24612e2dc6b2d09210d550654f0b8175a4
abbr <wenfred@gmail.com>
2014-12-20 01:01:35 +0000

    removed semicolons from all JS

--- app.js
+++ app.js
@@ -1,98 +1,98 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var stripJsonComments = require('strip-json-comments');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var fs = require('fs')
+var stripJsonComments = require('strip-json-comments')
 
-var routes = require('./routes/index');
-routes.publicFolderNm = 'public';
-routes.combineJs = true;
-var app = express();
+var routes = require('./routes/index')
+routes.publicFolderNm = 'public'
+routes.combineJs = true
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, routes.publicFolderNm));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, routes.publicFolderNm))
+app.set('view engine', 'ejs')
 
 // trust proxy
-app.set('trust proxy', true);
+app.set('trust proxy', true)
 
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
+// app.use(favicon(__dirname + '/public/img/favicon.ico'))
+app.use(logger('dev'))
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
-}));
-app.use(cookieParser());
+}))
+app.use(cookieParser())
 
 //CORS and Referer validation
-var clientWhiteList = path.join(__dirname, 'client-whitelist.json');
-var validClients = [/.*/];
+var clientWhiteList = path.join(__dirname, 'client-whitelist.json')
+var validClients = [/.*/]
 var parseWhiteList = function() {
     try {
         validClients = JSON.parse(stripJsonComments(fs.readFileSync(clientWhiteList).toString())).map(function(v) {
-            return new RegExp(v);
-        });
+            return new RegExp(v)
+        })
     }
     catch (err) {}
-};
-parseWhiteList();
+}
+parseWhiteList()
 fs.watchFile(clientWhiteList, function(curr, prev) {
     if (curr.mtime.getTime() > prev.mtime.getTime()) {
-        parseWhiteList();
+        parseWhiteList()
     }
-});
+})
 
 app.use(function(req, res, next) {
     // Referer
     if (req.headers.referer) {
         if (!validClients.some(function(v) {
-                return v.test(req.headers.referer);
+                return v.test(req.headers.referer)
             }) && req.headers.referer.indexOf(req.protocol + "://" + req.hostname) !== 0) {
-            return res.status(403).end();
+            return res.status(403).end()
         }
     }
 
     if (req.headers.origin) {
         if (validClients.some(function(v) {
-                return v.test(req.headers.origin);
+                return v.test(req.headers.origin)
             }) || req.headers.origin.indexOf(req.protocol + "://" + req.hostname) === 0) {
-            res.header('Access-Control-Allow-Origin', req.headers.origin);
+            res.header('Access-Control-Allow-Origin', req.headers.origin)
             if (req.headers['access-control-request-method']) {
-                res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+                res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method'])
             }
             if (req.headers['access-control-request-headers']) {
-                res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+                res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers'])
             }
             // preflight caching age set to 1 day
-            res.header('Access-Control-Max-Age', 86400);
-            // intercept preflight OPTIONS method
+            res.header('Access-Control-Max-Age', 86400)
+                // intercept preflight OPTIONS method
             if (req.method === 'OPTIONS') {
-                return res.status(200).end();
+                return res.status(200).end()
             }
         }
         else {
-            return res.status(403).end();
+            return res.status(403).end()
         }
     }
-    next();
-});
+    next()
+})
 
 app.use(express.static(path.join(__dirname, routes.publicFolderNm, 'assets'), {
     'index': false
-}));
+}))
 
-app.use('/', routes);
+app.use('/', routes)
 
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
 
 /// error handlers
 
@@ -100,25 +100,25 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
+        res.status(err.status || 500)
         res.render('api/error', {
             message: err.message,
             error: err,
             title: 'error'
-        });
-    });
+        })
+    })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('api/error', {
         message: err.message,
         error: {},
         title: 'error'
-    });
-});
+    })
+})
 
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,84 +1,84 @@
-var express = require('express');
-var router = express.Router();
-var recursive = require('recursive-readdir');
-var fs = require('fs');
-var path = require('path');
-var async = require('async');
-var ejs = require('ejs');
-var etag = require('etag');
-var deasync = require('deasync');
+var express = require('express')
+var router = express.Router()
+var recursive = require('recursive-readdir')
+var fs = require('fs')
+var path = require('path')
+var async = require('async')
+var ejs = require('ejs')
+var etag = require('etag')
+var deasync = require('deasync')
 var fileCompare = function(v1, v2) {
-    if (typeof v1 !== 'string' || typeof v2 !== 'string') throw "invalid argument";
-    var leveDiff = v1.split(path.sep).length - v2.split(path.sep).length;
+    if (typeof v1 !== 'string' || typeof v2 !== 'string') throw "invalid argument"
+    var leveDiff = v1.split(path.sep).length - v2.split(path.sep).length
     switch (true) {
         case (leveDiff !== 0):
-            return leveDiff;
+            return leveDiff
         default:
-            return v1.localeCompare(v2);
+            return v1.localeCompare(v2)
     }
-};
+}
 
 
 // Get combined.js
 router.get('*/combined.js', function(req, res) {
-    var thisFileUrlPath = req.params[0].replace(/\/$/, '');
-    res.type('application/javascript');
-    var jsPath = path.join(__dirname, '../', router.publicFolderNm, 'assets', thisFileUrlPath, '/js');
+    var thisFileUrlPath = req.params[0].replace(/\/$/, '')
+    res.type('application/javascript')
+    var jsPath = path.join(__dirname, '../', router.publicFolderNm, 'assets', thisFileUrlPath, '/js')
     recursive(path.resolve(jsPath), function(err, files) {
         if (!files) {
-            return res.status(404).end();
+            return res.status(404).end()
         }
-        files.sort(fileCompare);
+        files.sort(fileCompare)
         async.map(files, fs.readFile, function(err, outputs) {
             if (err) {
-                console.error(err);
-                res.status(500).end();
-                return;
+                console.error(err)
+                res.status(500).end()
+                return
             }
-            var cnt = '';
+            var cnt = ''
             outputs.forEach(function(output, idx) {
-                output = output.toString();
+                output = output.toString()
                 if (path.extname(files[idx]) === '.ejs') {
                     output = ejs.render(output, {
                         "unippearHost": req.protocol + "://" + req.hostname,
                         "thisFileUrlPath": path.dirname(files[idx]).substring(path.join(__dirname, '../', router.publicFolderNm, 'assets').length)
-                    });
+                    })
                 }
-                cnt += output + '\n';
-            });
-            res.setHeader('ETag', etag(cnt));
-            res.setHeader('Cache-Control', 'max-age=0');
+                cnt += output + '\n'
+            })
+            res.setHeader('ETag', etag(cnt))
+            res.setHeader('Cache-Control', 'max-age=0')
             if (req.fresh) {
-                res.status(304).end();
+                res.status(304).end()
             }
             else {
-                res.end(cnt);
+                res.end(cnt)
             }
-        });
-    });
-});
+        })
+    })
+})
 
 
 /* GET home page. */
 router.get(/^(.*)\/(index\.js)?$/, function(req, res) {
-    var thisFileUrlPath = req.params[0].replace(/\/$/, '');
-    res.type('application/javascript');
-    var cssPath = path.join(__dirname, '../', router.publicFolderNm, '/assets', thisFileUrlPath, '/css');
+    var thisFileUrlPath = req.params[0].replace(/\/$/, '')
+    res.type('application/javascript')
+    var cssPath = path.join(__dirname, '../', router.publicFolderNm, '/assets', thisFileUrlPath, '/css')
     recursive(cssPath, function(err, files) {
         var cssFiles = (files || []).map(function(v) {
-            return req.protocol + "://" + req.hostname + thisFileUrlPath + v.substring(cssPath.length - 4).replace(/\.ejs$/, '');
-        });
+            return req.protocol + "://" + req.hostname + thisFileUrlPath + v.substring(cssPath.length - 4).replace(/\.ejs$/, '')
+        })
 
-        cssFiles.sort(fileCompare);
-        var jsFiles = ['combined.js'];
+        cssFiles.sort(fileCompare)
+        var jsFiles = ['combined.js']
         if (!router.combineJs) {
-            var jsPath = path.join(__dirname, '../', router.publicFolderNm, '/assets', thisFileUrlPath, '/js/');
-            var drecursive = deasync(recursive);
+            var jsPath = path.join(__dirname, '../', router.publicFolderNm, '/assets', thisFileUrlPath, '/js/')
+            var drecursive = deasync(recursive)
             try {
                 jsFiles = (drecursive(jsPath) || []).map(function(v) {
-                    return 'js/' + v.substring(jsPath.length).replace(/\.ejs$/, '');
-                });
-                jsFiles.sort(fileCompare);
+                    return 'js/' + v.substring(jsPath.length).replace(/\.ejs$/, '')
+                })
+                jsFiles.sort(fileCompare)
             }
             catch (err) {}
         }
@@ -88,36 +88,36 @@ router.get(/^(.*)\/(index\.js)?$/, function(req, res) {
             "cssFiles": cssFiles,
             "jsFiles": jsFiles
         }, function(err, cnt) {
-            res.setHeader('ETag', etag(cnt));
-            res.setHeader('Cache-Control', 'max-age=0');
+            res.setHeader('ETag', etag(cnt))
+            res.setHeader('Cache-Control', 'max-age=0')
             if (req.fresh) {
-                res.status(304).end();
+                res.status(304).end()
             }
             else {
-                res.end(cnt);
+                res.end(cnt)
             }
-        });
-    });
-});
+        })
+    })
+})
 
 
 // Get all ejs assets
 router.get('*/*', function(req, res, next) {
-    var thisFileUrlPath = req.params[0].replace(/\/$/, '');
+    var thisFileUrlPath = req.params[0].replace(/\/$/, '')
 
     require('fs').exists(path.join(__dirname, '..', router.publicFolderNm, 'assets', req.path + '.ejs'), function(exists) {
         if (exists) {
-            res.type(path.extname(req.path));
-            res.setHeader('Cache-Control', 'max-age=0');
+            res.type(path.extname(req.path))
+            res.setHeader('Cache-Control', 'max-age=0')
             res.render(path.join('assets', req.path + '.ejs'), {
                 unippearHost: req.protocol + "://" + req.hostname,
                 "thisFileUrlPath": thisFileUrlPath
-            });
+            })
         }
         else {
-            next();
+            next()
         }
-    });
-});
+    })
+})
 
-module.exports = router;
+module.exports = router
